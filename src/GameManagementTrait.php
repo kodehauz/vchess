15734e003dc650de7200ea2eebaf16ab28d64339
Nathaniel John <david.nathaniel13@gmail.com>
2017-12-05 22:10:12 +0100

    Updated opponent game, random game and challenge game form

--- src/GameManagementTrait.php
+++ src/GameManagementTrait.php
@@ -20,7 +20,7 @@ trait GameManagementTrait {
    * @param \Drupal\user\UserInterface $black_user
    *
    */
-  public static function initializeGame(Game $game, UserInterface $white_user, UserInterface $black_user, UserInterface $challenger) {
+  public static function initializeGame(Game $game, UserInterface $white_user, UserInterface $black_user, UserInterface $challenger, $game_time, $time_per_move) {
     $white = GamerStatistics::loadForUser($white_user);
     $black = GamerStatistics::loadForUser($black_user);
 
@@ -33,22 +33,25 @@ trait GameManagementTrait {
     $game
       ->setWhiteUser($white_user)
       ->setBlackUser($black_user)
+      ->setWhiteTimeLeft($game_time)
+      ->setBlackTimeLeft($game_time)
+      ->setTimePerMove($time_per_move)
       ->setChallenger($challenger)
       ->setStatus(GamePlay::STATUS_IN_PROGRESS);
 
     $game->save();
   }
 
-  public static function createChallenge(UserInterface $user, $white_time, $black_time, $white_time_per_move, $black_time_per_move, $board_position) {
+  public static function createChallenge(UserInterface $user, $game_time, $time_per_move, $board_position) {
     $game = Game::create();
     $game
       ->setPlayerRandomly($user)
-      ->setBlackTimePerMove($time_per_move)
+      ->setTimePerMove($time_per_move)
       ->setStatus(GamePlay::STATUS_AWAITING_PLAYERS)
       ->setBoard($board_position)
       //Timer value for white and black players
-      ->setBlackTimeLeft($black_time)
-      ->setWhiteTimeLeft($white_time)
+      ->setBlackTimeLeft($game_time)
+      ->setWhiteTimeLeft($game_time)
       //Timer value ends
       ->setChallenger($user)
       ->save();
